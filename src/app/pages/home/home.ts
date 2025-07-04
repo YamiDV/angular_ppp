bdede880d44c7073a41fb945afded36ea05a505a
Jamie Vilchez Giraldo <yamivritra@gmail.com>
2025-07-04 15:13:54 -0500

    IV2

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, AfterViewInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrls: ['./home.css']
 })
-export class Home {
+export class Home implements AfterViewInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const cards = document.querySelectorAll('.card-home');
+      const overlay = document.getElementById('overlay');
+
+      cards.forEach(card => {
+        card.addEventListener('click', () => {
+          cards.forEach(c => c.classList.remove('active'));
+          card.classList.add('active');
+          overlay?.classList.add('active');
+        });
+      });
+
+      overlay?.addEventListener('click', () => {
+        cards.forEach(c => c.classList.remove('active'));
+        overlay?.classList.remove('active');
+      });
+    }
+  }
 }
